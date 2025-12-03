d858ba5da6b2a02cc9da36c5cc5cbfe9d1cf4f4b
ManoloEsS <manolo.estrada.s@gmail.com>
2025-12-03 15:37:25 -0700

    Add day3-2

---
+++ day3/part2/batteries_2.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+const input = "input.txt"
+
+func main() {
+	var joltages []int
+
+	f, err := os.Open(input)
+	if err != nil {
+		log.Fatalf("could not read file: %s", err)
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		var lineJoltage string
+		var startIndex, highestIndex int
+
+		for remaining := 11; remaining >= 0; remaining-- {
+			highestBatt := '0'
+
+			for index, num := range line[startIndex : len(line)-remaining] {
+				if num > highestBatt {
+					highestBatt = num
+					highestIndex = startIndex + index
+				}
+			}
+
+			lineJoltage += string(highestBatt)
+			startIndex = highestIndex + 1
+		}
+
+		intLineJoltage, err := strconv.Atoi(lineJoltage)
+		if err != nil {
+			log.Fatalf("could not convert lineJoltage to int: %s", err)
+		}
+		joltages = append(joltages, intLineJoltage)
+	}
+
+	totalJoltage := 0
+	for _, j := range joltages {
+		totalJoltage += j
+	}
+
+	fmt.Println(totalJoltage)
+}
